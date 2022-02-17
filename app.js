708ad36950701659b7e91485d68be1cb595d79c9
Warejali <waraj.com@gmail.com>
2022-02-17 11:57:26 +0600

    finish without revision

--- app.js
+++ app.js
@@ -1,45 +1,56 @@
-/* Function for input */
 
+/* Function for input */
 function userInput(inputNumber) {
-     /* Total Income */
     const amount = document.getElementById(inputNumber)
-    const amountText = amount.value; 
-    const totalAmount = parseFloat(amountText);
-    // amount.value = "";
-    return totalAmount
+    if (inputNumber > 0) {
+        const amountText = amount.value; 
+        const totalAmount = parseFloat(amountText);
+        return totalAmount
+    }
+    else{
+        document.getElementById('alart-message').style.display = 'block'
+        document.getElementById('account-section').style.display = 'none'
+    }
+}
+
+/* Functions for total cost */
+function expenses() {
+    const foodCost = userInput('food-cost');
+    const rentCost = userInput('rent-cost');
+    const clothesCost = userInput('clothes-cost');
+    const totalCost = foodCost + rentCost + clothesCost;
+    return totalCost
 }
 
 
 /* The Function for calculate Button */
 document.getElementById('calculate').addEventListener('click', function () {
-    const totalIncom = userInput('total-income');
-    const foodCost = userInput('food-cost');
-    const rentCost = userInput('rent-cost');
-    const clothesCost = userInput('clothes-cost');
-    const totalCost = foodCost + rentCost + clothesCost;
-
-    document.getElementById('total-expenses').innerText = totalCost;
-
-    const balance = totalIncom - totalCost;
+    const totalIncome = userInput('total-income');
+    
+    /* Total expenses */
+    const totalExpenses = expenses();
+    document.getElementById('total-expenses').innerText = totalExpenses;
+    
+    /* After expenses Balance */
+    const balance = totalIncome - totalExpenses;
     document.getElementById('balance').innerText = balance;
 })
 
 /* The Function for save Button */
 document.getElementById('save-btn').addEventListener('click', function() {
-    const totalIncom = userInput('total-income');
-
-    const saveInput = userInput('save-amount');
-    const totalSaveAmount = (saveInput /100) * totalIncom;
-
-    document.getElementById('saving-amount').innerText = totalSaveAmount;
-    /* Remaing balance */
-    const foodCost = userInput('food-cost');
-    const rentCost = userInput('rent-cost');
-    const clothesCost = userInput('clothes-cost');
+    const totalIncome = userInput('total-income');
     
-    const totalCost = foodCost + rentCost + clothesCost;
-    const totalCostWithSave = totalCost + totalSaveAmount;
-    const remainingBalance = totalIncom - totalCostWithSave;
+    /* Save Anount */
+    const saveInput = userInput('save-amount');
+    const totalSaveAmount = (saveInput /100) * totalIncome;
+    document.getElementById('saving-amount').innerText = totalSaveAmount;
+    
+    /* Remaing balance */
+    const totalExpenses = expenses();
+    const totalCostWithSave = totalExpenses + totalSaveAmount;
+    const remainingBalance = totalIncome - totalCostWithSave;
 
     document.getElementById('remaining-balance').innerText = remainingBalance;
 })
+
+
